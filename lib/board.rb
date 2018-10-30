53bcbe286dfbbd829197263eae4db42102e4d56c
Saint-Jean Germila <germila@noukod.com>
2018-10-30 14:00:20 +0000

    automatically backed up by learn

---
+++ lib/board.rb
@@ -0,0 +1,19 @@
+class Board
+
+  attr_accessor :cells
+
+
+def initialize
+@cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+end
+
+#def reset!
+#board.cells = ["X"]
+#board.reset!
+#end
+
+      
+
+end
+
+end
